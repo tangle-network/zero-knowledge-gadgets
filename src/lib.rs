c696190941c6f6933410949c3e859ab931b7289b
Filip Lazovic <lazoviccorp@gmail.com>
2021-05-21 17:08:06 +0200

    prelude update

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub mod test_data;
 
 pub mod prelude {
 	pub use ark_bls12_381::Fr as Bls381;
-	pub use ark_ff::{self, fields::PrimeField, to_bytes};
+	pub use ark_ff;
 	pub use ark_std;
 	pub use webb_crypto_primitives;
 }
