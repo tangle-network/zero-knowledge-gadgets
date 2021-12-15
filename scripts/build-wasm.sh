6b742a8fe450051b49e50d1373eccc6f17f9f4f7
mfaghihi <32965896+mfaghihi@users.noreply.github.com>
2021-12-15 08:10:47 -0500

    circom verifier tests  (#73)

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,1 +1,1 @@
-cargo build --target wasm32-unknown-unknown --release --no-default-features
+cargo build --target wasm32-unknown-unknown --release --no-default-features --workspace --exclude arkworks-circom-verifier
