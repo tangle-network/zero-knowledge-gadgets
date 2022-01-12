42bf3eed61a01ad463d8576f6572c3d8fdd0ac85
Todd Norton <toddn@commonwealth.im>
2022-01-12 11:56:52 +0100

    remove rehashing of leaves, other small fixes

--- arkworks-gadgets/src/merkle_tree/simple_merkle.rs
+++ arkworks-gadgets/src/merkle_tree/simple_merkle.rs
@@ -9,31 +9,30 @@ use std::{collections::BTreeSet, marker::PhantomData};
 #[derive(Clone)]
 pub struct Path<F: PrimeField, H: FieldHasher<F>, const N: usize> {
 	pub(crate) path: [(F, F); N],
-	_marker: PhantomData<H>, // Do we need to include this as part of the path?
+	_marker: PhantomData<H>,
 }
 
 impl<F: PrimeField, H: FieldHasher<F>, const N: usize> Path<F, H, N> {
-	/// Takes in an expected `root_hash` and raw input data (i.e. private
-	/// inputs) for a leaf and checks that the leaf belongs to a tree having the
-	/// expected hash.
+	/// Takes in an expected `root_hash` and leaf-level data (i.e. hashes of
+	/// secrets) for a leaf and checks that the leaf belongs to a tree having
+	/// the expected hash.
 	pub fn check_membership(&self, root_hash: &F, leaf: &F, hasher: &H) -> Result<bool, Error> {
 		let root = self.calculate_root(leaf, hasher)?;
 		Ok(root == *root_hash)
 	}
 
-	/// Assumes leaf is raw input, i.e. private inputs whose hash is stored
-	/// at leaf level
+	/// Assumes leaf contains leaf-level data, i.e. hashes of secrets
+	/// stored on leaf-level.
 	pub fn calculate_root(&self, leaf: &F, hasher: &H) -> Result<F, Error> {
-		let claimed_leaf_hash = hasher.hash(&[*leaf])?;
-		if claimed_leaf_hash != self.path[0].0 && claimed_leaf_hash != self.path[0].1 {
-			panic!("Leaf is not on path");
+		if *leaf != self.path[0].0 && *leaf != self.path[0].1 {
+			return Err(Error::from("Leaf is not on path"));
 		}
 
-		let mut prev = claimed_leaf_hash;
+		let mut prev = *leaf;
 		// Check levels between leaf level and root
 		for &(ref left_hash, ref right_hash) in &self.path {
 			if &prev != left_hash && &prev != right_hash {
-				panic!("Path nodes are not consistent");
+				return Err(Error::from("Path nodes are not consistent"));
 			}
 			prev = hasher.hash_two(left_hash, right_hash)?;
 		}
@@ -41,15 +40,15 @@ impl<F: PrimeField, H: FieldHasher<F>, const N: usize> Path<F, H, N> {
 		Ok(prev)
 	}
 
-	/// Given leaf raw input data determine what the index of this leaf must be
+	/// Given leaf data determine what the index of this leaf must be
 	/// in the Merkle tree it belongs to.  Before doing so check that the leaf
 	/// does indeed belong to a tree with the given `root_hash`
 	pub fn get_index(&self, root_hash: &F, leaf: &F, hasher: &H) -> Result<F, Error> {
 		if !self.check_membership(root_hash, leaf, hasher)? {
-			panic!("Leaf is not in the path");
+			return Err(Error::from("Leaf is not on path"));
 		}
 
-		let mut prev = hasher.hash(&[*leaf])?;
+		let mut prev = *leaf;
 		let mut index = F::zero();
 		let mut twopower = F::one();
 		// Check levels between leaf level and root
@@ -78,21 +77,20 @@ pub struct SparseMerkleTree<F: PrimeField, H: FieldHasher<F>, const N: usize> {
 }
 
 impl<F: PrimeField, H: FieldHasher<F>, const N: usize> SparseMerkleTree<F, H, N> {
-	/// Takes a collection of raw leaf data, hashes each one, inserts
+	/// Takes a collection of leaf data (hashes of secrets), inserts
 	/// these hashes at leaf level, and propagates the changes up the tree to
 	/// the root.
 	pub fn insert_batch(&mut self, leaves: &BTreeMap<u32, F>, hasher: &H) -> Result<(), Error> {
-		let last_level_index: u64 = (1u64 << self.empty_hashes.len()) - 1;
+		let last_level_index: u64 = (1u64 << N) - 1;
 
 		let mut level_idxs: BTreeSet<u64> = BTreeSet::new();
 		for (i, leaf) in leaves {
 			let true_index = last_level_index + (*i as u64);
-			let leaf_hash = hasher.hash(&[*leaf])?;
-			self.tree.insert(true_index, leaf_hash);
+			self.tree.insert(true_index, *leaf);
 			level_idxs.insert(parent(true_index).unwrap());
 		}
 
-		for level in 0..self.empty_hashes.len() {
+		for level in 0..N {
 			let mut new_idxs: BTreeSet<u64> = BTreeSet::new();
 			for i in level_idxs {
 				let left_index = left_child(i);
@@ -149,7 +147,10 @@ impl<F: PrimeField, H: FieldHasher<F>, const N: usize> SparseMerkleTree<F, H, N>
 	}
 
 	pub fn root(&self) -> F {
-		self.tree.get(&0).cloned().unwrap()
+		self.tree
+			.get(&0)
+			.cloned()
+			.unwrap_or(*self.empty_hashes.last().unwrap())
 	}
 
 	/// Give the path leading from the leaf at `index` up to the root.  This is
@@ -260,9 +261,9 @@ mod test {
 
 		let empty_hashes =
 			gen_empty_hashes::<Fq, BLSHash, HEIGHT>(&poseidon, default_leaf).unwrap();
-		let hash1 = poseidon.hash(&leaves[0..1]).unwrap();
-		let hash2 = poseidon.hash(&leaves[1..2]).unwrap();
-		let hash3 = poseidon.hash(&leaves[2..3]).unwrap();
+		let hash1 = leaves[0];
+		let hash2 = leaves[1];
+		let hash3 = leaves[2];
 
 		let hash12 = poseidon.hash_two(&hash1, &hash2).unwrap();
 		let hash34 = poseidon.hash_two(&hash3, &empty_hashes[0]).unwrap();
