ccb1a22fda3e1f900e2853c2e7641b9aac6b1302
Filip Lazovic <lazoviccorp@gmail.com>
2022-01-17 14:52:21 +0100

    Updated scripts to compile each feature isolated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,6 @@
-cargo build --release --all-features 
+# Build each crate with isolated features
+cargo build --release --no-default-features
+cargo build --release --no-default-features --features r1cs
+cargo build --release --no-default-features --features default_poseidon
+cargo build --release --no-default-features --features default_mimc
+cargo build --release --all-features
