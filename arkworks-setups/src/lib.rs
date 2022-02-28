3b13f338b95750fdfafa940ebad2a5b135974006
Drew Stone <drewstone329@gmail.com>
2022-02-28 13:59:11 -0500

    Add arkworks-setup package for hacking

---
+++ arkworks-setups/src/lib.rs
@@ -0,0 +1,40 @@
+use ark_ec::PairingEngine;
+use arkworks_gadgets::poseidon::field_hasher::{FieldHasher};
+use ark_crypto_primitives::Error;
+use ark_std::{
+	rand::{CryptoRng, RngCore},
+	vec::Vec,
+};
+use arkworks_circuits::setup::common::MixerProof;
+use arkworks_circuits::setup::common::Leaf;
+use arkworks_utils::utils::common::Curve;
+
+#[cfg(feature="r1cs")]
+pub mod r1cs;
+
+#[cfg(feature="plonk")]
+pub mod plonk;
+
+trait MixerProver<E: PairingEngine, H: FieldHasher<E::Fr>, const HEIGHT: usize> {
+    // For creating leaves where we supply the secret and the nullifier, for generating new values, pass None
+    fn create_leaf_with_privates<R: RngCore + CryptoRng>(
+        curve: Curve,
+        secret: Option<Vec<u8>>,
+        nullifier: Option<Vec<u8>>,
+        rng: &mut R,
+    ) -> Result<Leaf, Error>;
+    // For making proofs
+    fn create_proof<R: RngCore + CryptoRng>(
+        curve: Curve,
+        secret: Vec<u8>,
+        nullifier: Vec<u8>,
+        leaves: Vec<Vec<u8>>,
+        index: u64,
+        recipient: Vec<u8>,
+        relayer: Vec<u8>,
+        fee: u128,
+        refund: u128,
+        pk: Vec<u8>,
+        rng: &mut R,
+    ) -> Result<MixerProof, Error>;
+}
---
+++ arkworks-setups/src/r1cs/mixer/mod.rs
@@ -0,0 +1,66 @@
+use ark_ec::PairingEngine;
+use ark_ff::PrimeField;
+use arkworks_gadgets::poseidon::field_hasher::{Poseidon, FieldHasher};
+use ark_bn254::{Fr as Bn254Fr, Bn254};
+use ark_std::marker::PhantomData;
+use ark_crypto_primitives::Error;
+use ark_std::{
+	rand::{CryptoRng, RngCore},
+	vec::Vec,
+};
+use arkworks_circuits::setup::common::MixerProof;
+use arkworks_circuits::setup::common::Leaf;
+use arkworks_utils::utils::common::Curve;
+
+use arkworks_gadgets::{
+	arbitrary::mixer_data::Input as MixerDataInput,
+	leaf::mixer::Private,
+	merkle_tree::Path,
+};
+
+use crate::MixerProver;
+
+pub fn create_leaf<F: PrimeField, H: FieldHasher<F>>(hasher: H, private: &Private<F>) -> Result<F, Error> {
+    let leaf = hasher.hash_two(&private.secret(), &private.nullifier())?;
+    Ok(leaf)
+}
+
+pub fn create_nullifier<F: PrimeField, H: FieldHasher<F>>(hasher: H, private: &Private<F>) -> Result<F, Error> {
+    let nullifier_hash = hasher.hash_two(&private.nullifier(), &private.nullifier())?;
+    Ok(nullifier_hash)
+}
+
+struct MixerR1CSProver<E: PairingEngine, H: FieldHasher<E::Fr>, const HEIGHT: usize> {
+    engine: PhantomData<E>,
+    hasher: PhantomData<H>,
+}
+
+impl<E: PairingEngine, H: FieldHasher<E::Fr>, const HEIGHT: usize> MixerProver<E, H, HEIGHT> for MixerR1CSProver<E, H, HEIGHT> {
+    fn create_leaf_with_privates<R: RngCore + CryptoRng>(
+        curve: Curve,
+        secret: Option<Vec<u8>>,
+        nullifier: Option<Vec<u8>>,
+        rng: &mut R,
+    ) -> Result<Leaf, Error> {
+        let leaf = MixerLeaf::<E::Fr, H, HEIGHT>::new(curve, secret, nullifier, rng)?;
+        Ok(Leaf::Mixer(leaf))
+    }
+
+    fn create_proof<R: RngCore + CryptoRng>(
+        curve: Curve,
+        secret: Vec<u8>,
+        nullifier: Vec<u8>,
+        leaves: Vec<Vec<u8>>,
+        index: u64,
+        recipient: Vec<u8>,
+        relayer: Vec<u8>,
+        fee: u128,
+        refund: u128,
+        pk: Vec<u8>,
+        rng: &mut R,
+    ) -> Result<MixerProof, Error> {
+        todo!()
+    }
+}
+
+type MixerR1CSProver_Bn254_Poseidon_30 = MixerR1CSProver<Bn254, Poseidon<Bn254Fr>, 30>;
