dcfc9bcb86b1c60c6c64a2131c744501e6fdb4eb
Todd Norton <93260651+GhostOfGauss@users.noreply.github.com>
2022-03-02 00:52:10 +0100

    Plonk Anchor Vanchor (#169)
    Co-authored-by: Drew Stone <drewstone329@gmail.com>

--- arkworks-plonk-circuits/src/lib.rs
+++ arkworks-plonk-circuits/src/lib.rs
@@ -1,7 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[macro_use]
+pub extern crate ark_std;
+
+pub mod anchor;
 pub mod merkle_tree;
 pub mod mixer;
 pub mod poseidon;
 pub mod set_membership;
 pub mod utils;
+pub mod vanchor;
