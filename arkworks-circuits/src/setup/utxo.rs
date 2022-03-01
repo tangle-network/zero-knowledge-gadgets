df8ec2af2cfd5e0addbd792ce55dfcac00b5c354
Filip Lazovic <lazoviccorp@gmail.com>
2022-03-01 13:34:32 +0100

    Moved encryption to Keypair gadget

--- arkworks-circuits/src/setup/utxo.rs
+++ arkworks-circuits/src/setup/utxo.rs
@@ -2,47 +2,32 @@ use crate::setup::common::{PoseidonCRH_x5_2, PoseidonCRH_x5_4};
 use ark_crypto_primitives::Error;
 use ark_ff::{to_bytes, PrimeField};
 use ark_std::{
-	convert::TryInto,
 	error::Error as ArkError,
 	rand::{CryptoRng, RngCore},
 	string::ToString,
 	vec::Vec,
 };
 use arkworks_gadgets::{
-	keypair::vanchor::Keypair,
+	keypair::vanchor::{Keypair, EncryptedData},
 	leaf::vanchor::{Private as LeafPrivateInput, Public as LeafPublicInput, VAnchorLeaf as Leaf},
 };
 use arkworks_utils::poseidon::PoseidonParameters;
-use crypto_box::{
-	aead::{generic_array::GenericArray, Aead, Payload},
-	generate_nonce, ChaChaBox, PublicKey, SecretKey,
-};
 
 #[derive(Debug)]
 pub enum UtxoError {
 	NullifierNotCalculated,
-	EncryptionFailed,
-	DecryptionFailed,
 }
 
 impl core::fmt::Display for UtxoError {
 	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
 		let msg = match self {
 			UtxoError::NullifierNotCalculated => "Nullifier not calculated".to_string(),
-			UtxoError::EncryptionFailed => "Utxo encryption failed".to_string(),
-			UtxoError::DecryptionFailed => "Utxo data decryption failed".to_string(),
 		};
 		write!(f, "{}", msg)
 	}
 }
 
 impl ArkError for UtxoError {}
-
-pub struct EncryptedUtxo {
-	pub nonce: Vec<u8>,
-	pub cypher_text: Vec<u8>,
-	pub ephemeral_pk: Vec<u8>,
-}
 
 #[derive(Clone, Copy)]
 pub struct Utxo<F: PrimeField> {
@@ -127,73 +112,17 @@ impl<F: PrimeField> Utxo<F> {
 		Ok(())
 	}
 
-	pub fn encrypt<R: RngCore + CryptoRng>(&self, rng: &mut R) -> Result<EncryptedUtxo, Error> {
-		// Generate new nonce
-		let nonce = generate_nonce(rng);
-
-		// Convert private key into bytes array
-		let private_key_bytes = to_bytes!(self.keypair.secret_key)?;
-		let mut sc_bytes = [0u8; 32];
-		for i in 0..sc_bytes.len() {
-			sc_bytes[i] = private_key_bytes[i];
-		}
-
-		// Generate public key from secret key
-		// QUESTION: Should we derive the public key with poseidon.hash(secret_key)?
-		let secret_key = SecretKey::from(sc_bytes);
-		let public_key = PublicKey::from(&secret_key);
-
-		// Generate ephemeral sk/pk
-		// QUESTION: What are those?
-		let ephemeral_sk = SecretKey::generate(rng);
-		let ephemeral_pk = PublicKey::from(&ephemeral_sk);
-
-		let my_box = ChaChaBox::new(&public_key, &ephemeral_sk);
-
+	pub fn encrypt<R: RngCore + CryptoRng>(&self, rng: &mut R) -> Result<EncryptedData, Error> {
 		// We are encrypting the amount and the blinding
 		let msg = to_bytes![self.leaf_private.amount, self.leaf_private.blinding]?;
 		// Encrypting the message
-		let ct = my_box
-			.encrypt(&nonce, Payload {
-				msg: &msg,
-				aad: &[],
-			})
-			.map_err::<Error, _>(|_| UtxoError::EncryptionFailed.into())?;
-		Ok(EncryptedUtxo {
-			nonce: nonce.as_slice().to_vec(),
-			cypher_text: ct,
-			ephemeral_pk: ephemeral_pk.as_bytes().to_vec(),
-		})
+		let enc_data = self.keypair.encrypt(&msg, rng)?;
+		Ok(enc_data)
 	}
 
-	pub fn decrypt(&self, encrypted_utxo: &EncryptedUtxo) -> Result<(Vec<u8>, Vec<u8>), Error> {
-		// Creating a secret key
-		let private_key_bytes = to_bytes![self.keypair.secret_key]?;
-		let mut sc_bytes = [0u8; 32];
-		for i in 0..sc_bytes.len() {
-			sc_bytes[i] = private_key_bytes[i];
-		}
-		let secret_key = SecretKey::from(sc_bytes);
-
-		// Making ephemeral public key from the encryption data
-		let eph_bytes = &encrypted_utxo.ephemeral_pk[..];
-		let ephemeral_pk_bytes: [u8; 32] = eph_bytes
-			.try_into()
-			.map_err(|_| UtxoError::DecryptionFailed)?;
-		let ephemeral_pk = PublicKey::from(ephemeral_pk_bytes);
-
-		let my_box = ChaChaBox::new(&ephemeral_pk, &secret_key);
-
-		// Converting nonce into proper type
-		let nonce = GenericArray::from_slice(&encrypted_utxo.nonce);
-
-		// Decrypt the cypher text, get the plaintext
-		let plaintext = my_box
-			.decrypt(&nonce, Payload {
-				msg: &encrypted_utxo.cypher_text,
-				aad: &[],
-			})
-			.map_err::<Error, _>(|_| UtxoError::DecryptionFailed.into())?;
+	pub fn decrypt(&self, data: &EncryptedData) -> Result<(Vec<u8>, Vec<u8>), Error> {
+		// Decrypting the message
+		let plaintext = self.keypair.decypt(data)?;
 
 		// First 32 bytes is amount
 		// Second 32 bytes is blinding
--- arkworks-gadgets/src/keypair/vanchor/mod.rs
+++ arkworks-gadgets/src/keypair/vanchor/mod.rs
@@ -1,9 +1,40 @@
 use ark_crypto_primitives::{Error, CRH};
 use ark_ff::{to_bytes, ToBytes};
-use ark_std::marker::PhantomData;
+use ark_std::{marker::PhantomData, rand::{RngCore, CryptoRng}, error::Error as ArkError, convert::TryInto};
+
+use crypto_box::{
+	aead::{generic_array::GenericArray, Aead, Payload},
+	generate_nonce, ChaChaBox, PublicKey, SecretKey,
+};
 
 #[cfg(feature = "r1cs")]
 pub mod constraints;
+
+#[derive(Debug)]
+pub enum KeypairError {
+	EncryptionFailed,
+	DecryptionFailed,
+	SecretKeyParseFailed,
+}
+
+impl core::fmt::Display for KeypairError {
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+		let msg = match self {
+			KeypairError::EncryptionFailed => "Data encryption failed".to_string(),
+			KeypairError::DecryptionFailed => "Data decryption failed".to_string(),
+			KeypairError::SecretKeyParseFailed => "Failed to parse secret key".to_string(),
+		};
+		write!(f, "{}", msg)
+	}
+}
+
+impl ArkError for KeypairError {}
+
+pub struct EncryptedData {
+	pub nonce: Vec<u8>,
+	pub cypher_text: Vec<u8>,
+	pub ephemeral_pk: Vec<u8>,
+}
 
 #[derive(Default, Debug, Copy)]
 pub struct Keypair<B: Clone + Copy + ToBytes, H: CRH> {
@@ -33,6 +64,73 @@ impl<B: Clone + Copy + ToBytes, H: CRH> Keypair<B, H> {
 	) -> Result<H::Output, Error> {
 		let bytes = to_bytes![self.secret_key.clone(), commitment, index]?;
 		H::evaluate(h_w4, &bytes)
+	}
+
+	pub fn encrypt<R: RngCore + CryptoRng>(&self, msg: &[u8], rng: &mut R) -> Result<EncryptedData, Error> {
+		// Generate new nonce
+		let nonce = generate_nonce(rng);
+
+		// Convert private key into bytes array
+		let secret_key_bytes = to_bytes!(self.secret_key)?;
+		let sc_bytes: [u8; 32] = secret_key_bytes
+			.try_into()
+			.map_err(|_| KeypairError::SecretKeyParseFailed)?;
+
+		// Generate public key from secret key
+		// QUESTION: Should we derive the public key with poseidon.hash(secret_key)?
+		let secret_key = SecretKey::from(sc_bytes);
+		let public_key = PublicKey::from(&secret_key);
+
+		// Generate ephemeral sk/pk
+		let ephemeral_sk = SecretKey::generate(rng);
+		let ephemeral_pk = PublicKey::from(&ephemeral_sk);
+
+		let my_box = ChaChaBox::new(&public_key, &ephemeral_sk);
+
+		// Encrypting the message
+		let ct = my_box
+			.encrypt(&nonce, Payload {
+				msg: &msg,
+				aad: &[],
+			})
+			.map_err::<Error, _>(|_| KeypairError::EncryptionFailed.into())?;
+
+		Ok(EncryptedData {
+			nonce: nonce.as_slice().to_vec(),
+			cypher_text: ct,
+			ephemeral_pk: ephemeral_pk.as_bytes().to_vec(),
+		})
+	}
+
+	pub fn decypt(&self, encrypted_data: &EncryptedData) -> Result<Vec<u8>, Error> {
+		// Creating a secret key
+		let secret_key_bytes = to_bytes![self.secret_key]?;
+
+		let sc_bytes: [u8; 32] = secret_key_bytes
+			.try_into()
+			.map_err(|_| KeypairError::SecretKeyParseFailed)?;
+		let secret_key = SecretKey::from(sc_bytes);
+
+		// Making ephemeral public key from the encryption data
+		let eph_bytes = &encrypted_data.ephemeral_pk[..];
+		let ephemeral_pk_bytes: [u8; 32] = eph_bytes
+			.try_into()
+			.map_err(|_| KeypairError::DecryptionFailed)?;
+		let ephemeral_pk = PublicKey::from(ephemeral_pk_bytes);
+
+		let my_box = ChaChaBox::new(&ephemeral_pk, &secret_key);
+
+		// Converting nonce into proper type
+		let nonce = GenericArray::from_slice(&encrypted_data.nonce);
+
+		// Decrypt the cypher text, get the plaintext
+		let plaintext = my_box
+			.decrypt(&nonce, Payload {
+				msg: &encrypted_data.cypher_text,
+				aad: &[],
+			})
+			.map_err::<Error, _>(|_| KeypairError::DecryptionFailed.into())?;
+		Ok(plaintext)
 	}
 }
 
@@ -96,5 +194,18 @@ mod test {
 		let ev_res = PoseidonCRH::evaluate(&params4, &inputs_signature).unwrap();
 		let signature = keypair.signature(&commitment, &index, &params4).unwrap();
 		assert_eq!(ev_res, signature);
+	}
+
+	#[test]
+	fn should_encrypt_decrypt() {
+		let rng = &mut test_rng();
+		let private_key = Fq::rand(rng);
+		let keypair = Keypair::<Fq, PoseidonCRH>::new(private_key.clone());
+
+		let msg = vec![1, 2, 3];
+		let encrypted_data = keypair.encrypt(&msg, rng).unwrap();
+		let plaintext = keypair.decypt(&encrypted_data).unwrap();
+
+		assert_eq!(plaintext, msg);
 	}
 }
