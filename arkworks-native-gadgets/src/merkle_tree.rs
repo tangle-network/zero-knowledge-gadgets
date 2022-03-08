bce5f5f15b22a25ca16bd218e0051014943b4151
Drew Stone <drewstone329@gmail.com>
2022-03-08 16:22:48 -0500

    Fix

--- arkworks-native-gadgets/src/merkle_tree.rs
+++ arkworks-native-gadgets/src/merkle_tree.rs
@@ -70,7 +70,8 @@
 //!
 //! // Create the tree with a default leaf of zero.
 //! let default_leaf = Fr::from(0u64);
-//! let smt = SparseMerkleTree::<Fr, Poseidon<Fr>, 30>::new(&pairs, &poseidon, default_leaf).unwrap();
+//! let smt =
+//!		SparseMerkleTree::<Fr, Poseidon<Fr>, 30>::new(&pairs, &poseidon, default_leaf).unwrap();
 //! ```
 
 use crate::poseidon::FieldHasher;
