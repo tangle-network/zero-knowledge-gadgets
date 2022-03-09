deb3683aacf55681982368152acca687143bca06
Levi Sledd <levi.sledd@protonmail.com>
2022-03-09 15:21:18 -0800

    Documentation for everything in native poseidon, except a usage example.

--- arkworks-native-gadgets/src/poseidon/sbox.rs
+++ arkworks-native-gadgets/src/poseidon/sbox.rs
@@ -1,10 +1,29 @@
+//! The S-box used in the Poseidon hash function.
+//! 
+//! The `PoseidonSbox` struct contains only one signed 8-bit integer.
+//! In the notation of 
+//! [the original Poseidon paper](https://eprint.iacr.org/2019/458.pdf),
+//! this is alpha.
+//! 
+//! The value of alpha can be either 3, 5, 17, or -1: the default is 5.
+//! Trying to use any other value will result in a `PoseidonError`.
+//! 
+//! The `apply_sbox` method takes an element of a prime field `F`
+//! and raises it to the power alpha (in `F`).
+//! 
+
+
+
+/// Importing dependencies
 use super::PoseidonError;
 use ark_ff::PrimeField;
 
-/// An S-Box that can be used with Poseidon.
+
+/// The PoseidonSbox struct contains just a public signed 8-bit integer.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct PoseidonSbox(pub i8);
 
+/// The default value for PoseidonSbox is 5.
 impl Default for PoseidonSbox {
 	fn default() -> Self {
 		PoseidonSbox(5)
@@ -12,6 +31,10 @@ impl Default for PoseidonSbox {
 }
 
 impl PoseidonSbox {
+
+	/// Takes in an element of a prime field and raises it to the power alpha (`sbox.0`)
+	/// within that field. The method assumes that alpha is either 3, 5, 17, or -1.
+	/// If not, it throws `PoseidonError`.
 	pub fn apply_sbox<F: PrimeField>(&self, elem: F) -> Result<F, PoseidonError> {
 		let res = match self.0 {
 			3 => elem * elem * elem,
@@ -27,7 +50,7 @@ impl PoseidonSbox {
 				sixteenth * elem
 			}
 			-1 => elem.inverse().ok_or(PoseidonError::ApplySboxFailed)?,
-			// default to cubed
+			
 			n => return Err(PoseidonError::InvalidSboxSize(n)),
 		};
 		Ok(res)
