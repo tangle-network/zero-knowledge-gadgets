f30a6cea2b47cb51acb3bf0ddf2d8f42872a7bf5
Filip Lazovic <lazoviccorp@gmail.com>
2022-03-15 16:29:32 +0100

    get_index

--- arkworks-setups/src/utxo.rs
+++ arkworks-setups/src/utxo.rs
@@ -8,6 +8,7 @@ use arkworks_native_gadgets::poseidon::{FieldHasher, Poseidon};
 pub enum UtxoError {
 	NullifierNotCalculated,
 	EncryptedDataDecodeError,
+	IndexNotSet
 }
 
 impl core::fmt::Display for UtxoError {
@@ -15,6 +16,7 @@ impl core::fmt::Display for UtxoError {
 		let msg = match self {
 			UtxoError::NullifierNotCalculated => "Nullifier not calculated".to_string(),
 			UtxoError::EncryptedDataDecodeError => "Failed to decode encrypted data".to_string(),
+			&UtxoError::IndexNotSet => "Utxo not set".to_string()
 		};
 		write!(f, "{}", msg)
 	}
@@ -136,5 +138,10 @@ impl<F: PrimeField> Utxo<F> {
 	pub fn get_nullifier(&self) -> Result<F, Error> {
 		self.nullifier
 			.ok_or(UtxoError::NullifierNotCalculated.into())
+	}
+
+	pub fn get_index(&self) -> Result<u64, Error> {
+		self.index
+			.ok_or(UtxoError::IndexNotSet.into())
 	}
 }
